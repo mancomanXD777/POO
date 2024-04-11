0f17e50ce1b799b1f08df42e3f4a349e7495989f
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-04-11 08:37:25 -0400

    ok

---
+++ ejercicio4/script4.js
@@ -0,0 +1,10 @@
+function contar(){
+    let contador = 20;
+    let array = []
+
+    while (contador >= 10){
+            array.push(contador);
+            contador--;
+    }
+var lista = parseFloat(document.getElementById('lista').value);
+}
