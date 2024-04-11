5779b18b08260d4a8495d815ac0d12318d92f82d
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-04-11 14:56:52 -0400

    ok

--- ejercicio4/script4.js
+++ ejercicio4/script4.js
@@ -2,7 +2,7 @@ function generarLista() {
 var lista = document.getElementById('lista');
   lista.innerHTML = ''; // Limpiar la lista antes de generar una nueva
 
-for (var i = 0; i <= 100; i += 5) {
+while (var i = 0; i <= 100; i += 5) {
     var listItem = document.createElement('li');
     listItem.appendChild(document.createTextNode(i));
     lista.appendChild(listItem);
