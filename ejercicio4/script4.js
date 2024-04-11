d0932fb83f8628f24bb713d489717aa8888b622f
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-04-11 09:22:23 -0400

    ok

--- ejercicio4/script4.js
+++ ejercicio4/script4.js
@@ -3,7 +3,7 @@ let lista = document.getElementById("lista");
 let contador = 0;
 
 while (contador <= 100) {
-    let elementoLista = document.createElement(`<p>${lista}<br></p>`);
+    let elementoLista = document.createElement("lista");
     elementoLista.appendChild(document.createTextNode(contador));
     lista.appendChild(elementoLista);
     contador += 5;
