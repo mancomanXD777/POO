7da7f83dc2d75efbb9c653193fc0ddaec7b6b659
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-04-11 15:15:59 -0400

    ok

--- ejercicio4/script4.js
+++ ejercicio4/script4.js
@@ -31,3 +31,23 @@ function generarterceraLista() {
     i += 2;
     }
 }
+
+function compararNumeros() {
+    // Obtener los valores ingresados por el usuario
+    var num1 = parseFloat(document.getElementById('numero1').value);
+    var num2 = parseFloat(document.getElementById('numero2').value);
+    var num3 = parseFloat(document.getElementById('numero3').value);
+    var num4 = parseFloat(document.getElementById('numero4').value);
+    var num5 = parseFloat(document.getElementById('numero5').value);
+
+    // Comparar los números
+    if (num1 > num2) (num1 > num3)(num1 > num4) (num1 > num5) (num2 > num1) (num2 > num3) (num2 > num4) (num2 > num5) (num3 > num1) (num3 > num2) (num3 > num4) (num3 > num5) (num3 > num2) (num3 > num2){
+    alert(num1 + " es mayor que " + num2);
+    alert(num2 + " es menor que " + num1);
+    } else if (num2 > num1) {
+    alert(num2 + " es mayor que " + num1);
+    alert(num1 + " es menor que " + num2);
+    } else {
+    alert("Los números son iguales");
+    }
+}
