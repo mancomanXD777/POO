a3028ab8e02cdf431f958df34d3348301b38a53a
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-04-11 08:48:23 -0400

    ok

--- ejercicio4/script4.js
+++ ejercicio4/script4.js
@@ -2,9 +2,9 @@ function contarwhile(){
     let contador = 20;
     let array = [];
 
-    while (contador > 0){
+    while (contador > 10){
             array.push(contador);
             contador--;
     }
-document.getElementById("lista").innerHTML = <p>contador del 20 al 10 con while</p>
+document.getElementById("lista").innerHTML = `<p>contador del 20 al 10 con while</p> <br><h2 > ${Array} </h2></br>`;
 }
