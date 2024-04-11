21476170dbaaf9e24d30332dc721fd6eab752cc2
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-04-11 08:50:16 -0400

    ok

--- ejercicio4/script4.js
+++ ejercicio4/script4.js
@@ -2,7 +2,7 @@ function contarwhile(){
     let contador = 20;
     let array = [];
 
-    while (contador > 10){
+    while (contador >= 10){
             array.push(contador);
             contador--;
     }
