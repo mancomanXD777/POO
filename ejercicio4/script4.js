d0f45887d0ab29365e5c38bba331c4df47eff2cb
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-04-12 10:20:34 -0400

    ok

--- ejercicio4/script4.js
+++ ejercicio4/script4.js
@@ -34,20 +34,16 @@ function generarterceraLista() {
 
 function compararNumeros() {
     // Obtener los valores ingresados por el usuario
-    var num1 = parseFloat(document.getElementById('numero1').value);
-    var num2 = parseFloat(document.getElementById('numero2').value);
-    var num3 = parseFloat(document.getElementById('numero3').value);
-    var num4 = parseFloat(document.getElementById('numero4').value);
-    var num5 = parseFloat(document.getElementById('numero5').value);
+    var num = [];
+    var numeromayor
 
-    // Comparar los números
-    if (num1 > num2) (num1 > num3)(num1 > num4) (num1 > num5) (num2 > num1) (num2 > num3) (num2 > num4) (num2 > num5) (num3 > num1) (num3 > num2) (num3 > num4) (num3 > num5) (num3 > num2) (num3 > num2){
-    alert(num1 + " es mayor que " + num2);
-    alert(num2 + " es menor que " + num1);
-    } else if (num2 > num1) {
-    alert(num2 + " es mayor que " + num1);
-    alert(num1 + " es menor que " + num2);
-    } else {
-    alert("Los números son iguales");
+    for (var i = 0; i < 5; i++) {
+        var numero = prompt("ingresa el numero " + (i+1));
+        compararNumeros.push(number(numero));
+
+        if (i === 0 || numero > numeromayor) {
+            numeromayor = numero;
+        }
     }
 }
+document.write("el numero mayo es: " + numeromayor)
