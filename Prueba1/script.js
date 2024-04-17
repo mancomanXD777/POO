92d06d76bdaf28d7ec80845007d9e6733cf31bd9
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-04-17 12:47:15 -0400

    ok

--- Prueba1/script.js
+++ Prueba1/script.js
@@ -1,28 +1,26 @@
 /* Recibir un arreglo numerico con numeros negativos y positivos todos los valores negativos remplazarlos por 0 */
 
 function noNegative(arr){
-
+    var lista = document.getElementById('lista'); // Mover la declaración de lista aquí para evitar repetición
     for(let i = 0; i < arr.length; i++){
-    var lista = document.getElementById('lista');
+        
         if(arr[i] < 0){
             arr[i] = 0;
 
         }
+        lista.innerHTML += '<li>' + arr[i] + '</li>'; // Agregar cada elemento del array como un nuevo ítem de lista
     }
 
 
 
 
 
-    
-    lista.innerHTML = ''; // Limpiar la lista antes de generar una nueva
-    lista.innerHTML += '<li>' + array[arr] + '</li>';
+
+
     return arr;
-};
-
-
+}
 
 var lista = document.getElementById('lista');
-let array = [-2,5,-6,10]
-noNegative(Array);
-
+let array = [-2, 5, -6, 10];
+lista.innerHTML = ''; // Limpiar la lista antes de generar una nueva
+noNegative(array); // Pasar el array como argumento en lugar de Array
