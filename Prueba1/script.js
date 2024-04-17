9479dbc9f221b6d956765e63eacc9f9ebee16b8c
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-04-17 12:21:46 -0400

    ok

--- Prueba1/script.js
+++ Prueba1/script.js
@@ -1,20 +1,20 @@
 /* Recibir un arreglo numerico con numeros negativos y positivos todos los valores negativos remplazarlos por 0 */
 
-function noNegative(){
+function noNegative(arr){
 
     for(let i = 0; i < arr.length; i++){
     var lista = document.getElementById('lista');
         if(arr[i] < 0){
             arr[i] = 0;
-
+            lista.innerHTML = ''; // Limpiar la lista antes de generar una nueva
+            lista.innerHTML += '<li>' + array[arr] + '</li>';
         }
     }
 
 
 
 
-    lista.innerHTML = ''; // Limpiar la lista antes de generar una nueva
-    lista.innerHTML += '<li>' + array[arr] + '</li>';
+
     return arr;
 };
 
