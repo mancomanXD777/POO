0d2454a46fa635e483563dc3a1e363328d714b48
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-05-23 08:47:44 -0400

    ok

--- ejercicio5/Manipulacion tablas/script.js
+++ ejercicio5/Manipulacion tablas/script.js
@@ -6,3 +6,13 @@ for (let i = 1; i <= 10; i++)  {
     const resultado = numero * i;
     console.log(`${numeros} X ${resultado}`);
 }
+
+const cadenaOriginal = prompt("ingresa una cadena de texto");
+const caracteresArray = cadenaOriginal.split("");
+for (let i = 0; i < Math.floor(caracteresArray.length / 2); i++){
+    const temp = caracteresArray[i];
+    caracteresArray[i] = caracteresArray[caracteresArray.length - 1 - i];
+    caracteresArray[caracteresArray.length - 1 - i] = temp;
+}
+const cadenaInvertida = caracteresArray.join("");
+console.log(`la cadena invertida es: ${cadenaInvertida}`);
