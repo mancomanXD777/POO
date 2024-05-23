75a340ac3bc26f5684ba269237caa989a4ab9d93
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-05-23 08:37:43 -0400

    ok

--- ejercicio5/Manipulacion tablas/script.js
+++ ejercicio5/Manipulacion tablas/script.js
@@ -1,5 +1,8 @@
-function traerTablas(){
-let num = parseInt(document.getElementById("inputText").value);
-alert(num);
+const numero = parseInt(prompt("ingresa un numero:"));
 
+console.log(`tabla de multiplicacion del ${numero}`);
+
+for (let i = 1; i <= 10; i++)  {
+    const resultado = numero * i;
+    console.log(`${numeros} X ${resultado}`);
 }
