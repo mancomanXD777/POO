39b37863cce23b0badea121e6489004daf67064a
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-05-23 09:08:09 -0400

    ok

--- ejercicio5/Manipulacion tablas/script.js
+++ ejercicio5/Manipulacion tablas/script.js
@@ -1,13 +1,16 @@
 const numero = parseInt(prompt("ingresa un numero:"));
 
-console.log(`tabla de multiplicacion del ${numero}`);
+const tablasMultiplicacionElement = document.getElementById("tabla-multiplicacion");
+
+tablasMultiplicacionElement.innerHTML = `<h2>tabla de multiplicacion del ${numero}:</h2>`;
 
 for (let i = 1; i <= 10; i++)  {
     const resultado = numero * i;
-    console.log(`${numeros} X ${resultado}`);
+    tablasMultiplicacionElement.innerHTML += `${numero} X ${i} = ${resultado}<br>`;
 }
 
 const cadenaOriginal = prompt("ingresa una cadena de texto");
+const cadenaInvertidaElement = document.getElementById("cadena-invertida");
 const caracteresArray = cadenaOriginal.split("");
 for (let i = 0; i < Math.floor(caracteresArray.length / 2); i++){
     const temp = caracteresArray[i];
@@ -15,4 +18,4 @@ for (let i = 0; i < Math.floor(caracteresArray.length / 2); i++){
     caracteresArray[caracteresArray.length - 1 - i] = temp;
 }
 const cadenaInvertida = caracteresArray.join("");
-console.log(`la cadena invertida es: ${cadenaInvertida}`);
+cadenaInvertidaElement.innerHTML = `<h2>La cadena Invertida es: ${cadenaInvertida}</h2>`;
