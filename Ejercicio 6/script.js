b11fab6824617f0122706c9b587d7b30445962be
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-06-06 09:21:21 -0400

    ok

--- Ejercicio 6/script.js
+++ Ejercicio 6/script.js
@@ -3,11 +3,12 @@
 function cambiarImagen1(elemento) {
 elemento.src = 'Chad.jpg';
 
-onclick=cambiarImagen1(elemento)
-elemento.src = 'tiste.jpg';
+
+    onclick=cambiarImagen(elemento)
+    elemento.src = 'tiste.jpg';
 
 
 
-onclick=cambiarImagen1(elemento)
-elemento.src = 'sorprendio.jpg';
-}
+        onclick=cambiarImagen(elemento)
+        elemento.src = 'sorprendio.jpg';
+        }
