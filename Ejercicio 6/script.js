3523c0cfb69d0a42039d19d0c53f7d3321db72ba
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-06-06 09:05:32 -0400

    ok

--- Ejercicio 6/script.js
+++ Ejercicio 6/script.js
@@ -3,7 +3,35 @@
 function ejemplo(elemento) {
 const MiImagen = document.querySelector('img1');
 
-MiImagen.src = 'img2.jpg';
+MiImagen.src = 'Chad.jpg';
 
     elemento.src = 'img2'
 }
+
+
+function ejemplo(elemento) {
+    const MiImagen = document.querySelector('Chad');
+    
+    MiImagen.src = 'tiste.jpg';
+    
+        elemento.src = 'img3'
+    }
+
+
+function ejemplo(elemento) {
+    const MiImagen = document.querySelector('tiste');
+    
+    MiImagen.src = 'sorprendio.jpg';
+    
+        elemento.src = 'img4'
+    }
+
+    
+function ejemplo(elemento) {
+    const MiImagen = document.querySelector('img4');
+    
+    MiImagen.src = 'Enojao.jpg';
+
+    
+        elemento.src = 'img1'
+    }
