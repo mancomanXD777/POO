69d785266be51be59dc77ffae607cdbc6e05702a
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-06-06 08:51:46 -0400

    ok

--- Ejercicio 6/script.js
+++ Ejercicio 6/script.js
@@ -1,5 +1,3 @@
 function ejemplo(elemento) {
-    console.log("elemento clickeado", elemento);
-    elemento.style.backgroundColor = 'yellow'; // Cambia el color de fondo del botón
-    elemento.textContent = 'Clickeado!'; // Cambia el texto del botón
+
 }
