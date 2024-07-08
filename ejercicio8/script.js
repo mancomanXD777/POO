a1e378ea7caf5ddfdfe7b51833bfae8a653d9572
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-07-08 10:51:25 -0400

    ok

--- ejercicio8/script.js
+++ ejercicio8/script.js
@@ -1,9 +1,10 @@
 function añadir_LI() {
     var lista = document.getElementById('lista');
-    lista.innerHTML = ''; // Limpiar la lista antes de generar una nueva
+
 
     i=input(Text)
     
-    lista.innerHTML += '<li>' + i + '</li>';
+    lista.innerHTML += '<br>';
+    lista.innerHTML += '<li>' + i + '</li>'
     
     }
