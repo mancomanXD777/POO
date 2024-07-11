199848d70064ca52d308e6df37d36ee717ca179e
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-07-11 14:39:23 -0400

    ok

--- ejercicio9/script.js
+++ ejercicio9/script.js
@@ -1,17 +1,9 @@
-function Boton() {
-// Seleccionar el botón y el elemento para mostrar el contador
-const clickButton = document.getElementById('clickButton');
-const clickCountSpan = document.getElementById('clickCount');
+const botonContador = document.getElementById('botonContador');
+const contador = document.getElementById('contador');
 
-// Inicializar el contador de clics
-let clickCount = 0;
+let clicks = 0;
 
-// Agregar el evento de clic al botón
-clickButton.addEventListener('click', () => {
-  // Incrementar el contador de clics
-clickCount++;
-
-// Actualizar el texto del elemento que muestra el contador
-clickCountSpan.textContent = clickCount;
+botonContador.addEventListener('click', function() {
+clicks++;
+contador.textContent = clicks;
 });
-}
