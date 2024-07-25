dcc1e82b802a44d7d364a0c403fd096fe3c2440f
MancomanXD777 <estebanibanez@liceovvh.cl>
2024-07-25 12:31:30 -0400

    ok

---
+++ ejercicio11/script.js
@@ -0,0 +1,10 @@
+function encontrarMayorMenor(){
+
+    acumuladorNotas = 0
+    for i in range(1,8,1) ;
+        nota1 = float(input('ingrese un numero: '))
+        acumuladorNotas1 = acumuladorNotas1 + nota1
+    promedio1 = (acumuladorNotas1)
+    
+    print (promedio1);
+}
---
+++ ejercicio12/script.js
@@ -0,0 +1,5 @@
+nombre_usuario= input('un gusto en conocerte. cual es tu nombre?')
+edad_usuario = int(input('cuantos años tienes '))
+
+print('encantado de conocerte' (nombre_usuario))
+print(f `tienes ${edad_usuario} años de edad` )
